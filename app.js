7983d60df20ea2b51b4e7faa74a736c9862b1543
kushagrx <kushagrabisht10@gmail.com>
2025-08-04 17:49:02 +0530

    Initializing the project

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello World!');
 })
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server started on port http://localhost:${port}`);
-});
-
-export default app;
+})
