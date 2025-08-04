fc4f3eb99f23d15c7afd6c47fc1af937cb4e5c8a
kushagrx <kushagrabisht10@gmail.com>
2025-08-04 18:13:31 +0530

    Fixed eslint config and created environment variable files

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,16 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    // Combine node and (optionally) browser globals:
+    languageOptions: {
+      globals: {
+        ...globals.node,   // Node.js globals (includes process, __dirname, etc.)
+        // ...globals.browser, // Uncomment if you want browser globals too
+      },
+    },
+  },
 ]);
